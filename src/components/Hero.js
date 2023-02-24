86d0e025733b275dde0eb9c6c379bbfc27585e4b
Ali Ebrahimi <74895984+CyrusM3@users.noreply.github.com>
2023-02-24 10:04:24 -0800

    updated about me

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,9 +12,7 @@ const Hero = () => {
                 <img src={heroImage} alt='img' />
               </div>
               <h1 className='home__title'>Ali Ebrahimi</h1>
-              <span className='home__profession'>
-                Junior Frontend Developer
-              </span>
+              <span className='home__profession'>Frontend Developer</span>
               <div className='home__social'>
                 {socialLinks.map((link) => {
                   const { id, href, icon } = link;
