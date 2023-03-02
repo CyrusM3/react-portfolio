52da00c91c7f90998d919d9d9f137bb5a4a21f65
Ali Ebrahimi <74895984+CyrusM3@users.noreply.github.com>
2023-03-01 18:55:08 -0800

    modified message notify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const successNotify = () =>
-  toast.success('Message Sent!', {
+  toast.success('Message Sent successfully.', {
     position: 'top-right',
     autoClose: 2000,
     hideProgressBar: true,
